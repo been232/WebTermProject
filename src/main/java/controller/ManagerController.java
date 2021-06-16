5b87a4bfcde2ac7b846d991310c0a0ae6190148b
been2 <nrjs3k@gmail.com>
2021-06-16 18:27:27 +0900

    Repository 추가

--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -1,4 +1,21 @@
 package controller;
 
-public class ManagerController {
+import service.ManagerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ManagerController implements Controller {
+    private final ManagerService booksManagerService = new ManagerService();
+
+    @Override
+    public ModelAndView process(HttpServletRequest requset, HttpServletResponse response, String url)
+        throws ServletException, IOException {
+        ModelAndView modelAndView = new ModelAndView();
+        if(url.equals(""));      // url 받기
+
+        return modelAndView;
+    }
 }
