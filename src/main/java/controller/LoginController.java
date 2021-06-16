ef51a9a1697ae64974ee2b1a8be9ca4b6228fc18
dpmoomin <75132800+dpmoomin@users.noreply.github.com>
2021-06-17 07:27:30 +0900

    로그인 작업 - 2
    로그인 및 세션 생성 구현 완료 작업중..

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -6,8 +6,15 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import java.io.IOException;
-import java.util.ArrayList;
+import service.LoginService;
 
 public class LoginController implements Controller {
     private final LoginService loginService = new LoginService();
@@ -17,7 +24,24 @@ public class LoginController implements Controller {
             throws ServletException, IOException {
         ModelAndView modelAndView = new ModelAndView();
         if(url.equals("/Login/login")) {
+            HttpSession session = request.getSession();
+            String id = "";
+            String pw = "";
+            int loginResult;
             modelAndView.setViewName("Login/login");
+            if(request.getMethod().equals("POST"))
+            {
+                id = request.getParameter("id");
+                pw = request.getParameter("pw");
+                loginResult = this.loginService.Login(id, pw);
+                if(loginResult == 1)
+                {
+                    session.setAttribute("userID", id);
+                    session.setAttribute("userName", "회원");
+                    System.out.println("로그인 성공");
+                    modelAndView.setViewName("main");
+                }
+            }
         }
         else {
             modelAndView.setStatus(HttpServletResponse.SC_NOT_FOUND);
