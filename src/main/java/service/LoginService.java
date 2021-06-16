982e9dcf3277b3f180d9085aa8a7ee103fdefb85
dpmoomin <75132800+dpmoomin@users.noreply.github.com>
2021-06-17 07:30:34 +0900

    Revert "로그인 작업 - 2"
    This reverts commit ef51a9a1697ae64974ee2b1a8be9ca4b6228fc18.

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,11 +1,6 @@
 package service;
 
-import persistance.LoginRepository;
-
-
 public class LoginService {
-    private final LoginRepository loginRepository = LoginRepository.getInstacne();
     public LoginService() {
     }
-    public int Login(String id, String pw){ return this.loginRepository.certificationLogin(id, pw); }
 }
