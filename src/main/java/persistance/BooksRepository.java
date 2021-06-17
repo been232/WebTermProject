5134c9c7175e7e98bc8635e9ea4e32efba982a3f
choigibome <rlqja5084@naver.com>
2021-06-17 13:19:25 +0900

    Books Insert by Admin without Management

--- src/main/java/persistance/BooksRepository.java
+++ src/main/java/persistance/BooksRepository.java
@@ -7,6 +7,9 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.awt.print.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class BooksRepository {
     private static BooksRepository instance;
@@ -28,6 +31,30 @@ public class BooksRepository {
         return instance;
     }
     public void save(Books books){
+        String sql = "insert into books(Book_Id, Book_Name,Book_Writer,Book_Publisher,Book_registration ) values(?,?,?,?,?);";
 
+        Connection conn =null;
+        PreparedStatement pstmt = null;
+        try{
+            conn = ds.getConnection();
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,books.getId());
+            pstmt.setString(2,books.getName());
+            pstmt.setString(3,books.getWriter());
+            pstmt.setString(4,books.getPublisher());
+            pstmt.setString(5,books.getRegistraion().toString());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                pstmt.close();
+                conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
